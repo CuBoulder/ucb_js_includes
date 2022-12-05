0439b6427b43bc317f182883dcf8fd3b5299be48
Pat <breepatbree@gmail.com>
2022-12-05 14:04:18 -0700

    wip: Form activates in Block library

--- src/Plugin/Block/IncludesBlock.php
+++ src/Plugin/Block/IncludesBlock.php
@@ -1,14 +1,20 @@
 <?php
 
-namespace Drupal\IncludesBlock\Plugin\Block;
+/**
+ * @file
+ * Contains \Drupal\ucb_js_includes\Plugin\Block\CampusNewsBlock.
+ */
 
+namespace Drupal\ucb_js_includes\Plugin\Block;
+
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Block\BlockBase;
 
 /**
  * Provides a Block.
  *
  * @Block(
- *   id = "IncludesBlock",
+ *   id = "includes_block",
  *   admin_label = @Translation("Includes Block"),
  *   category = @Translation("External JavaScript"),
  * )
@@ -30,6 +36,28 @@ class IncludesBlock extends BlockBase {
       ],
       // Attach our third party JS, as defined in the module's libraries.yml file.
     //   '#attached' => ['library' => ['library_name/component']],
+    ];
+  }
+
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['includes_block'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Who'),
+      '#description' => $this->t('Who do you want to say hello to?'),
+      '#default_value' => $this->configuration['includes_block'],
+    ];
+
+    return $form;
+  }
+
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $this->configuration['includes_block'] = $values['includes_block'];
+  }
+
+  public function defaultConfiguration() {
+    return [
+      'IncludesBlock' => $this->t(''),
     ];
   }
 
