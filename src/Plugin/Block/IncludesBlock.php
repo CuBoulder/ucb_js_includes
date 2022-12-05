fb18c07eb562c1fcfd394b47032ea4d4ae9f996e
Pat <breepatbree@gmail.com>
2022-12-05 15:22:50 -0700

    wip: form has conditional subfields for each respective includes type'

--- src/Plugin/Block/IncludesBlock.php
+++ src/Plugin/Block/IncludesBlock.php
@@ -41,12 +41,88 @@ class IncludesBlock extends BlockBase {
 
   public function blockForm($form, FormStateInterface $form_state) {
     $form['includes_block'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Who'),
-      '#description' => $this->t('Who do you want to say hello to?'),
-      '#default_value' => $this->configuration['includes_block'],
-    ];
+			'#type'           => 'radios',
+			'#title'          => $this->t('Include Block Type'),
+			'#options'        => [
+				'Add AdmitHub Include' =>$this->t('AdmitHub'),
+				'Add LiveChat Include' =>$this->t('LiveChat'),
+				'Add Slate Page Include' =>$this->t('Slate'),
+				'Add StatusPage Include' =>$this->t('StatusPage')
+			],
+      '#attributes' => [
+        //define static name and id so we can easier select it
+        // 'id' => 'include_block_type',
+        'name' => 'field_include_block_type',
+      ],
+			'#description'    => t('Create an Include Block for the selected type')
+		];
 
+    $form['includes_block_ah_license'] = [
+      '#type' => 'textfield',
+      '#size' => '60',
+      '#placeholder' => 'License ID',
+      '#states' => [
+        //show this textfield only if the AdmitHub is selected above
+        'visible' => [
+          ':input[name="field_include_block_type"]' => ['value' => 'Add AdmitHub Include'],
+        ],
+      ],
+    ];
+    $form['includes_block_ah_college'] = [
+      '#type' => 'textfield',
+      '#size' => '60',
+      '#placeholder' => 'College ID',
+      '#states' => [
+        //show this textfield only if the AdmitHub is selected above
+        'visible' => [
+          ':input[name="field_include_block_type"]' => ['value' => 'Add AdmitHub Include'],
+        ],
+      ],
+    ];
+    $form['includes_block_lc_license'] = [
+      '#type' => 'textfield',
+      '#size' => '60',
+      '#placeholder' => 'License ID',
+      '#states' => [
+        //show this textfield only if the LiveChat is selected above
+        'visible' => [
+          ':input[name="field_include_block_type"]' => ['value' => 'Add LiveChat Include'],
+        ],
+      ],
+    ];
+    $form['includes_block_slate_form_id'] = [
+      '#type' => 'textfield',
+      '#size' => '60',
+      '#placeholder' => 'Slate Form ID',
+      '#states' => [
+        //show this textfield only if the Slate is selected above
+        'visible' => [
+          ':input[name="field_include_block_type"]' => ['value' => 'Add Slate Page Include'],
+        ],
+      ],
+    ];
+    $form['includes_block_slate_domain'] = [
+      '#type' => 'textfield',
+      '#size' => '60',
+      '#placeholder' => 'Slate Domain',
+      '#states' => [
+        //show this textfield only if the Slate is selected above
+        'visible' => [
+          ':input[name="field_include_block_type"]' => ['value' => 'Add Slate Page Include'],
+        ],
+      ],
+    ];
+    $form['includes_block_sp_url'] = [
+      '#type' => 'textfield',
+      '#size' => '60',
+      '#placeholder' => 'StatusPage.io URL',
+      '#states' => [
+        //show this textfield only if the AdmitHub is selected above
+        'visible' => [
+          ':input[name="field_include_block_type"]' => ['value' => 'Add StatusPage Include'],
+        ],
+      ],
+    ];
     return $form;
   }
 
